a8f04a11bc1e60fe2cc4e3a1276eb8419dae8ab9
Frank Freisberg <f.freisberg@telekom.de>
2020-03-24 14:32:30 +0100

    erster Aufschlag Screen Create a Ride

---
+++ app/src/main/java/com/example/carpoolwireframes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.carpoolwireframes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
